23722fe788f9abc78bc856e33f37bdfe002a6068
Ahmed Yasser <ahmedyasserofficial@gmail.com>
2025-01-19 23:35:35 +0200

    fixed cors

--- server/index.js
+++ server/index.js
@@ -10,6 +10,14 @@ dotenv.config();
 const env = process.env.environemnt;
 const app = express();
 
+// CORS setup - Allow only specific origin(s)
+const corsOptions = {
+  origin: "http://localhost:4200",
+  methods: "GET,POST,PUT,DELETE",
+  allowedHeaders: "Content-Type,Authorization",
+  credentials: true,
+};
+
 cloudinary.config({
   cloud_name: "da4vjfd6j",
   api_key: "596459554247427",
@@ -22,8 +30,9 @@ mongoose
   .then(() => console.log("db connected"))
   .catch((e) => console.log("fucking error: " + e));
 
-app.use(express.json());
-app.use(cors());
+app.use(express.json({ limit: "50mb" }));
+app.use(express.urlencoded({ limit: "50mb", extended: true }));
+app.use(cors(corsOptions));
 app.use("/api/auth", authRoute);
 app.use("/api/products", productRoute);
 
